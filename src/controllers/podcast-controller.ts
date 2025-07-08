bf7d2d9a40e66006110789274bf997a871736510
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-07-08 16:32:22 -0300

    create controller

---
+++ src/controllers/podcast-controller.ts
@@ -0,0 +1,23 @@
+import {IncomingMessage,ServerResponse} from 'http'
+
+export const getListEpisodes = async (req:IncomingMessage,res:ServerResponse) =>{
+  res.writeHead(200,{'content-type':'application/json'})
+  res.end(JSON.stringify([
+  {
+    "podcastName": "flow",
+    "episode": "CBUM - Flow #319",
+    "videoId": "pQSuQmUfS30",
+    "cover": "https://i.ytimg.com/vi/pQSuQmUfS30/maxresdefault.jpg",
+    "link": "https://www.youtube.com/watch?v=pQSuQmUfS30",
+    "categories": ["saúde", "esporte", "bodybuilder"]
+  },
+  {
+    "podcastName": "flow",
+    "episode": "RUBENS BARRICHELLO - Flow #339",
+    "videoId": "4KDGTdiOV4I",
+    "cover": "https://i.ytimg.com/vi/4KDGTdiOV4I/maxresdefault.jpg",
+    "link": "https://www.youtube.com/watch?v=4KDGTdiOV4I",
+    "categories": ["esporte", "corrida"]
+  }
+]))
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import * as http from 'http'
-
-const server = http.createServer((request: http.IncomingMessage,response: http.ServerResponse) => {})
+import {getListEpisodes} from './controllers/podcast-controller'
+const server = http.createServer(async (req: http.IncomingMessage,res: http.ServerResponse) => {
+  if(req.method === 'GET') {
+    await getListEpisodes(req,res)
+  }
+})
 const PORT = process.env.PORT
-server.listen(PORT || 3333, ()=>{
-  console.log(`Server is running on port ${PORT}`)
+server.listen(process.env.PORT || 3333, ()=>{
+  console.log(`Server is running on port ${process.env.PORT}`)
 })
