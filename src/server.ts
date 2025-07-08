79dca200c5ee9606dd4b1291b9608ca9a84aba4f
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-07-08 12:49:26 -0300

    create server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,7 @@
-console.log('Hello World')
+import * as http from 'http'
+
+const server = http.createServer((request: http.IncomingMessage,response: http.ServerResponse) => {})
+const PORT = process.env.PORT
+server.listen(PORT || 3333, ()=>{
+  console.log(`Server is running on port ${PORT}`)
+})
